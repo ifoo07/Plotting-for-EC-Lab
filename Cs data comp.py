0a03dae60e3af43b8d4d02e928268af54acdbc6b
ifoo07 <ifoohabib@gmail.com>
2018-11-17 12:03:09 +0200

    Update Cs data comp.py

--- Cs data comp.py
+++ Cs data comp.py
@@ -13,7 +13,7 @@ import numpy as np
 fig = figure(figsize = (8,6), dpi=150)
 path = r'D:\\Google Drive\\Masters Research - Supercapacitor\\Data\\EC data\\2 Electrode\\Swagelok T-Type\\\comp data\\'
 labels = ['Pristine MX', '5% OLC','10% OLC']
-sname = ['Pristine MX ESR data', '4% OLC ESR data','8% OLC ESR data']
+sname = ['Pristine MX Cs data', '4% OLC Cs data','8% OLC Cs data']
 
 fname=[]
 for i in sname:
@@ -26,8 +26,8 @@ for j in range(len(sname)):
     ax.plot(CD,Cs,marker='o', label = labels[j])
 
 ax.set_xlabel('Current Density [A/g]')
-ax.set_ylabel(r'ESR [$\Omega$]')
+ax.set_ylabel(r'Specific Capacitance [F/g]')
 ax.legend()
 ax.grid()
-savefig(path +'ESR data comp new.png')
+savefig(path +'Cs data comp new.png')
 show()
